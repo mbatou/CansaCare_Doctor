6836eaa4c05dc4c3e68bb86418069c8cababfc53
Georges Mbatou Charles DIEME <georgesmbatoucharles@gmail.com>
2023-03-15 18:25:09 +0000

    Add Appointment to the list of doctors
    - unable the recyclerView in FragmentAppointments
    - create the adapter to display and populate the appointments
    - create a model for appointment aligned to the created in patient app
    -
    - update the appointment model from patient side to take patient name
    - add methods to fetch the appointments

--- app/src/main/java/com/eagle/cansacaredoctor/adapter/AppointmentAdapter.java
+++ app/src/main/java/com/eagle/cansacaredoctor/adapter/AppointmentAdapter.java
@@ -1,6 +1,7 @@
 package com.eagle.cansacaredoctor.adapter;
 
 import android.content.Intent;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,10 +13,76 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.eagle.cansacaredoctor.AppointmentOpened;
 import com.eagle.cansacaredoctor.R;
 import com.eagle.cansacaredoctor.ressources.Appointment;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.List;
+import java.util.Objects;
 
 
-public class AppointmentAdapter{
+public class AppointmentAdapter extends RecyclerView.Adapter<AppointmentAdapter.AppointmentViewHolder>{
 
+    private final List<Appointment> appointments;
+    private final CollectionReference databaseReferenceStore = FirebaseFirestore.getInstance().collection("User");
+    String appointmentPatientName;
+
+
+    public AppointmentAdapter(List<Appointment> appointments) {
+        this.appointments = appointments;
+    }
+
+    @NonNull
+    @Override
+    public AppointmentAdapter.AppointmentViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.appointment_card, parent, false);
+        return new AppointmentAdapter.AppointmentViewHolder(view);    }
+
+    @Override
+    public void onBindViewHolder(@NonNull AppointmentAdapter.AppointmentViewHolder holder, int position) {
+
+        // Get the post at the current position
+        Appointment appointment = appointments.get(position);
+
+            // Bind the data to the views
+        holder.appointmentTimeView.setText(appointment.getTime());
+        holder.appointmentDate.setText(appointment.getDate());
+//        holder.appointmentType.setText(appointment.getType());
+        holder.appointmentPatientName.setText(appointment.getPatientName());
+
+
+        // Set click listener for the whole item view
+        holder.itemView.setOnClickListener(v -> {
+            // Create intent for the new activity
+            Intent intent = new Intent(v.getContext(), AppointmentOpened.class);
+//            intent.putExtra("content", notification.getContent());
+//            intent.putExtra("author", notification.getAuthorId());
+//            intent.putExtra("time", notification.getPostTime());
+//            intent.putExtra("postId", notification.getId());
+
+            // Start the new activity
+            v.getContext().startActivity(intent);
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return appointments.size();
+    }
+
+    static class AppointmentViewHolder extends RecyclerView.ViewHolder{
+
+        TextView appointmentTimeView, appointmentDate;
+        TextView appointmentPatientName;
+
+
+
+        public AppointmentViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            appointmentPatientName =itemView.findViewById(R.id.appointment_card_patient_name);
+            appointmentTimeView =itemView.findViewById(R.id.appointment_time);
+            appointmentDate =itemView.findViewById(R.id.appointment_date);
+        }
+    }
 }
--- app/src/main/java/com/eagle/cansacaredoctor/fragmentsmain/FragmentAppointments.java
+++ app/src/main/java/com/eagle/cansacaredoctor/fragmentsmain/FragmentAppointments.java
@@ -1,18 +1,36 @@
 package com.eagle.cansacaredoctor.fragmentsmain;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.eagle.cansacaredoctor.R;
+import com.eagle.cansacaredoctor.adapter.AppointmentAdapter;
+import com.eagle.cansacaredoctor.ressources.Appointment;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class FragmentAppointments extends Fragment {
+
+    private final List<Appointment> appointments = new ArrayList<>(); // Define the list of appointments
+
+    DatabaseReference mDatabase;
 
     public FragmentAppointments() {
         // Required empty public constructor
@@ -22,12 +40,40 @@ public class FragmentAppointments extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        mDatabase = FirebaseDatabase.getInstance().getReference("appointments");
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_appointments, container, false);
+        View view = inflater.inflate(R.layout.fragment_appointments, container, false);
+        RecyclerView recyclerView = view.findViewById(R.id.appointments_recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        AppointmentAdapter appointmentAdapter = new AppointmentAdapter(appointments); // create a new instance of AppointmentAdapter
+        recyclerView.setAdapter(appointmentAdapter); // set the adapter for the RecyclerView
+
+        // Read the appointments from Firebase Realtime Database
+        mDatabase.addValueEventListener(new ValueEventListener() {
+            @SuppressLint("NotifyDataSetChanged")
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                appointments.clear();
+                for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
+                    Appointment appointment = dataSnapshot.getValue(Appointment.class);
+                    appointments.add(appointment);
+                }
+                appointmentAdapter.notifyDataSetChanged(); // notify the adapter about the data changes
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Log.e("TAG", "onCancelled", error.toException());
+            }
+        });
+
+        return view;
     }
 }
